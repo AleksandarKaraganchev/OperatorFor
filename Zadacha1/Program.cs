2bacd5b07c3abb55f3d1f75dacad0bedc64a8e80
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2023-11-15 18:31:59 +0200

    Add project files.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    int result = i * j;
+                    Console.WriteLine($"{i} * {j} = {result}");
+                }
+            }
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Сумата на числата от 1 до 100 е: {sum}");
+        }
+    }
+}
